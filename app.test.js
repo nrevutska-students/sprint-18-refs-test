1b324d8157e6a90423f76667460842d433541d43
nrevutska <46501186+nrevutska@users.noreply.github.com>
2023-03-06 17:49:54 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,22 +1,7 @@
-import React from 'react';
-import ReactDOM, { unmountComponentAtNode } from 'react-dom';
-import '@testing-library/jest-dom/extend-expect';
+import { render } from "@testing-library/react";
 
-import App from '../components/app/app.js';
+import App from "../components/app/app.js";
 
-let container = null;
-
-beforeEach(() => {
-  container = document.createElement("div");
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it('Renders App without crashing', () => {
-  container = document.createElement('div');
-  ReactDOM.render(<App />, container);
+it("Renders App without crashing", () => {
+  render(<App />);
 });
